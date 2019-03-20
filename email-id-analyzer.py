c2e9c148e2811e253d3e6f555a986cf589e7a237
Devesh Chande <33889891+DeveshChande@users.noreply.github.com>
2019-03-20 22:15:25 +0530

    Add files via upload

---
+++ email-id-analyzer.py
@@ -0,0 +1,56 @@
+"""
+Email-ID-Analyzer, by Devesh Chande
+This is a simple email-id checker which attempts to notify the user whether
+his/her email-id has been disclosed via the haveibeenpwned API.
+It will notify the user of the following information, in the event that a
+breach has occurred :
+
+1. Company Name
+2. Compromised information
+3. Summary of the breach
+
+In the event that the email is not leaked (as far as verification through public
+resources go, the user will be notified that no data leaks have been associated
+with that email-id.)
+
+"""
+
+import requests
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+
+    def handle_data(self, data):
+        print(data)
+
+
+user_agent = 'Email-ID-Analyzer-DC'
+
+print("Welcome to your personal Email-ID-Analyzer!")
+print("-------------------------------------------")
+
+print("Please enter the email-id you wish to check.")
+user_email_id = str(input())
+
+print(f'Check whether the email-id : {user_email_id} has been comprised...')
+
+r = requests.get(f'https://haveibeenpwned.com/api/breachedaccount/{user_email_id}')
+
+
+
+if r.status_code == 404:
+    print("Horray! The email-id has not been compromised!")
+else:
+    print("Pwned! The email-id has been leaked. Change your passwords immediately!")
+    print("--------------------------------")
+    company_list = r.json()
+    i = 0
+    while i < len(company_list):
+        print(f'Name : {company_list[i]}')
+        r1 = requests.get(f'https://haveibeenpwned.com/api/v2/breach/{company_list[i]}')
+        temp_list = r1.json()
+        print(f'Website: {temp_list["Domain"]}')
+        print(f'What was leaked : {temp_list["DataClasses"]}')
+        print(f'Date of breach : {temp_list["BreachDate"]}')
+        print('---------------------------------------------------------------')
+        i+=1
